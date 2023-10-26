2f85701dc997e7266329a78affba4dbdb8ea8ac2
Yevgen Nykytenko <yevgen.nykytenko@aspose.com>
2023-10-26 09:50:53 +0300

    Simplified setEncoding

--- Examples/AdvancedUsage/Loading/LoadOptionsByDocumentType/Csv/convertCsvBySpecifyingEncoding.js
+++ Examples/AdvancedUsage/Loading/LoadOptionsByDocumentType/Csv/convertCsvBySpecifyingEncoding.js
@@ -1,12 +1,9 @@
-const java = require('java')
-java.options.push('-Dfile.encoding=UTF8')
-const Charset = java.import('java.nio.charset.Charset')
 /**
  * This example demonstrates how to convert a csv document to pdf with advanced options
  */
 async function convertCsvBySpecifyingEncoding(groupdocs, inputFilePath) {
   const loadOptions = new groupdocs.conversion.CsvLoadOptions()
-  loadOptions.setEncoding(Charset.forName("Shift_JIS"))
+  loadOptions.setEncoding("Shift_JIS")
 
   const outputPath = `${groupdocs.outputFolder}/ConvertCsvBySpecifyingEncoding.pdf`
   
--- Examples/AdvancedUsage/Loading/LoadOptionsByDocumentType/Txt/convertTxtBySpecifyingEncoding.js
+++ Examples/AdvancedUsage/Loading/LoadOptionsByDocumentType/Txt/convertTxtBySpecifyingEncoding.js
@@ -1,13 +1,10 @@
-const java = require('java')
-java.options.push('-Dfile.encoding=UTF8')
-const Charset = java.import('java.nio.charset.Charset')
 /**
 * This example demonstrates how to convert a txt document to pdf with advanced options
 */
 async function convertTxtBySpecifyingEncoding(groupdocs, inputFilePath) {
   const loadOptions = new groupdocs.conversion.TxtLoadOptions()
-  loadOptions.setEncoding(Charset.forName("Shift_JIS"))
-
+  loadOptions.setEncoding("Shift_JIS")
+  
   const outputPath = `${groupdocs.outputFolder}/ConvertTxtBySpecifyingEncoding.pdf`
   
   const converter = new groupdocs.conversion.Converter(inputFilePath, loadOptions)
